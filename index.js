73d2fb10eb3b1622125fca49aeaeb045da719cb4
RelancioBorgesDev <relancio.dev@gmail.com>
2022-03-03 16:11:36 -0300

    Alura - Node - 03

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const chalk = require('chalk');
+const fs = require('fs');
 
-console.log(chalk.blue('vamos começar!'));
-
-const paragrafo = 'Texto retornado por uma função';
-
-function texto(string) {
-  return string;
+function trataErro(erro){
+  throw new Error(chalk.red(erro.code))
 }
 
-console.log(texto(paragrafo));
+async function pegaArquivo(caminho){
+  const encodig = 'utf-8';
+  try{  
+    const texto = await fs.promises.readFile(caminho, encoding)
+    console.log(chalk.green(texto))
+  }catch(erro){
+    trataErro(erro)
+  }
+  
+
+}
+
+/* function pegaArquivo (caminhoDoArquivo){
+  const encoding = 'utf-8'
+  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+   fs.promises
+   .readFile(caminhoDoArquivo, encoding,)
+   .then((texto) => console.log(texto))
+   .then((erro) => trataErro(erro))
+
+  })
+} */
+
+pegaArquivo('./arquivos/texto1.md')
